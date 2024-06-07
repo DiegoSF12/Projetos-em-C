c134768f5493554f9b48f81b918f98d892ee2547
Dieguim <diegofranciscodesouza7@gmail.com>
2024-06-07 12:57:33 -0300

    Atualização da implementação de Grafos e Listas Dinamicas

--- ListasDinamicas.c
+++ ListasDinamicas.c
@@ -1,11 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 typedef struct NO{
     int dados;
-    NO *prox;
+    struct NO* prox;
 }NO;
+
+typedef struct Lista{
+    NO* ini;
+    NO* fim;
+}Lista;
+
+Lista Lista1 = {NULL, NULL};
+
+void insereNoInicio(int elemento){
+    NO *ptr = (NO*) malloc (sizeof(NO));
+    if(ptr == NULL){
+        printf("ERRO: Alocação de memoria não foi realizado");
+    }
+    else{
+        ptr->dados = elemento;
+        ptr->prox = NULL;
+        if(Lista1.ini == NULL){
+            Lista1.fim = ptr;
+        }
+        else{
+            ptr->prox = Lista1.ini;
+        }
+        Lista1.ini = ptr;
+    }
+}
+
+void insereNoFinal(int elemento){
+    NO *ptr = (NO*) malloc (sizeof(NO));
+    if(ptr == NULL){
+        printf("ERRO: Falha ao alocar memoria");
+    }
+    else{
+        ptr->dados = elemento;
+        ptr->prox = NULL;
+        if(Lista1.ini == NULL){
+            Lista1.ini = ptr;
+            Lista1.fim = ptr;
+        }
+        else{
+            Lista1.fim -> prox = ptr;
+            Lista1.fim = ptr;
+        }    
+    }
+}
+
+void menu(){
+    printf("1 - Para inserir no inicio");
+    printf("2 - ");
+    printf();
+    printf();
+}
+
+
 
 int main (){
     
